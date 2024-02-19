ea7db73dfe45c56cb0cace5b545782afb354206c
parth adhikari <parthadhikari1812@gmail.com>
2024-02-19 18:59:34 +0530

    added crud routes

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.getRoutes, name="routes"),
     path('notes/',views.getNotes, name="notes"),
+    path('notes/create',views.createNote, name="create-note"),
+    path('notes/<str:rid>/update',views.updateNote, name="update-note"),
+    path('notes/<str:rid>/delete',views.deleteNote, name="delete-note"),
     path('notes/<str:rid>/',views.getNote, name="note"),
 ]
