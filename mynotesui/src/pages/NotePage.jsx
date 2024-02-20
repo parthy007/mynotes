dff008dc60f3214ff9c8f3416254a5efb0c80b54
parth adhikari <parthadhikari1812@gmail.com>
2024-02-21 00:49:37 +0530

    added frontend

---
+++ mynotesui/src/pages/NotePage.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { ReactComponent as ArrowLeft } from "../assets/arrow-left.svg";
+
+const NotePage = () => {
+  const { id } = useParams();
+  const [note, setNote] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (id === "new") {
+      setNote({});
+    } else {
+      getNote();
+    }
+  }, [id]);
+
+  const getNote = async () => {
+    let res = await fetch(`/api/notes/${id}/`);
+    let data = await res.json();
+    setNote(data);
+  };
+
+  const updateNote = async () => {
+    await fetch(`/api/notes/${id}/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  const createNote = async () => {
+    await fetch(`/api/notes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(note),
+    });
+  };
+
+  const deleteNote = async () => {
+    await fetch(`/api/notes/${id}/`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    navigate("/");
+  };
+
+  const handleSubmit = () => {
+    if (id !== "new" && note.body === "") {
+      deleteNote();
+    } else if (id !== "new") {
+      updateNote();
+    } else if (id === "new" && note.body !== "") {
+      createNote();
+    }
+    navigate("/");
+  };
+
+  return (
+    <div className="note">
+      <div className="note-header">
+        <h3>
+          <ArrowLeft onClick={handleSubmit} />
+        </h3>
+        {id !== "new" ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
+      </div>
+      <textarea
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        value={note?.body}
+      ></textarea>
+    </div>
+  );
+};
+
+export default NotePage;
